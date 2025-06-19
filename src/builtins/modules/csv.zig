280e0b5c163178fa40456e49c4cd77bd57e2c5bd
trevorswan11 <tcs94@case.edu>
2025-06-18 20:20:21 -0400

    worked on csv

--- src/builtins/modules/csv.zig
+++ src/builtins/modules/csv.zig
@@ -12,12 +12,68 @@ const BuiltinModuleHandler = builtins.BuiltinModuleHandler;
 
 const pack = builtins.pack;
 
+fn expectStringArgs(
+    allocator: std.mem.Allocator,
+    args: []const *ast.Expr,
+    env: *Environment,
+    count: usize,
+) ![]const []const u8 {
+    const writer_err = driver.getWriterErr();
+
+    if (args.len != count) {
+        try writer_err.print("csv module: expected {d} argument(s), got {d}\n", .{ count, args.len });
+        return error.ArgumentCountMismatch;
+    }
+
+    var result = std.ArrayList([]const u8).init(allocator);
+    defer result.deinit();
+
+    for (args, 0..) |arg, idx| {
+        const val = try eval.evalExpr(arg, env);
+        if (val != .string) {
+            try writer_err.print("csv module: expected a string, got a {s} @ arg {d}\n", .{@tagName(val), idx});
+            return error.TypeMismatch;
+        }
+        try result.append(val.string);
+    }
+
+    return try result.toOwnedSlice();
+}
+
 pub fn load(allocator: std.mem.Allocator) !Value {
-    const map = std.StringHashMap(Value).init(allocator);
+    var map = std.StringHashMap(Value).init(allocator);
+
+    try pack(&map, "read", readHandler);
+    try pack(&map, "read", writeHandler);
+    try pack(&map, "read", parseHandler);
+    try pack(&map, "read", stringifyHandler);
 
     return .{
         .object = map,
     };
+}
+
+fn readHandler(allocator: std.mem.Allocator, args: []const *ast.Expr, env: *Environment) anyerror!Value {
+    const func_args = try expectStringArgs(allocator, args, env, 1);
+    const filepath = func_args[0];
+    const contents = try std.fs.cwd().readFileAlloc(allocator, filepath, 1 << 20);
+    defer allocator.free(contents);
+
+    return try parseCSV(allocator, contents);
+}
+
+fn writeHandler(allocator: std.mem.Allocator, args: []const *ast.Expr, env: *Environment) anyerror!Value {
+    const func_args = try expectStringArgs(allocator, args, env, 2);
+    const filepath = try func_args[0];
+    const contents = stringifyCSV(allocator, func_args[1]);
+    try std.fs.cwd().openFile(sub_path: []const u8, flags: OpenFlags)
+
+}
+
+fn parseHandler(allocator: std.mem.Allocator, args: []const *ast.Expr, env: *Environment) anyerror!Value {
+}
+
+fn stringifyHandler(allocator: std.mem.Allocator, args: []const *ast.Expr, env: *Environment) anyerror!Value {
 }
 
 // === TESTING ===
