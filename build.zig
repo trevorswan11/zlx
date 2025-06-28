7ac72bf5a9970682b72ce4fe14bb8c386d766ebe
trevorswan11 <tcs94@case.edu>
2025-06-27 22:08:30 -0400

    trying build change fro linux

--- build.zig
+++ build.zig
@@ -32,6 +32,7 @@ pub fn build(b: *std.Build) void {
     const raylib_dep = b.dependency("raylib_zig", .{
         .target = target,
         .optimize = optimize,
+        .linux_display_backend = .X11,
     });
 
     const raylib = raylib_dep.module("raylib");
